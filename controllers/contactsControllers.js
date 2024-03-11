21d558a5521a390a45e1dfcf88f4d77be1854f2a
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-03-11 12:54:05 +0000

    add pagination favorite

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -4,9 +4,15 @@ import HttpError from "../helpers/HttpError.js";
 export const getAllContacts = async (req, res, next) => {
   try {
     const { _id: owner } = req.user;
-    const { page = 1, limit = 20 } = req.query;
+    const { page = 1, limit = 20, favorite } = req.query;
     const skip = (page - 1) * limit;
-    const result = await Contact.find({ owner })
+    let query = { owner };
+
+    if (favorite !== undefined) {
+      query.favorite = favorite === "true";
+    }
+
+    const result = await Contact.find(query)
       .skip(skip)
       .limit(limit)
       .populate("owner", "name email");
