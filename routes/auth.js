fd2139bbc3076953f15a26b6fe2ccd339f94a012
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-03-13 18:30:43 +0000

    create

--- routes/auth.js
+++ routes/auth.js
@@ -7,8 +7,10 @@ import {
   getCurrent,
   logout,
   updateSubscription,
+  updateAvatars,
 } from "../controllers/auth.js";
 import { authenticate } from "../middlewares/authenticate.js";
+import { upload } from "../middlewares/upload.js";
 
 const authRouter = express.Router();
 authRouter.post("/register", validateBody(signupSchema), register);
@@ -16,4 +18,10 @@ authRouter.post("/login", validateBody(loginSchema), login);
 authRouter.get("/current", authenticate, getCurrent);
 authRouter.post("/logout", authenticate, logout);
 authRouter.patch("/", authenticate, updateSubscription);
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  updateAvatars
+);
 export default authRouter;
